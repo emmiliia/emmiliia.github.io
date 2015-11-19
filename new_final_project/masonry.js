888c08ffa700da07005c7971739a897bd872dfc9
emmiliia <emmi-liia@torkkujajanokkosia.com>
2015-11-19 11:08:46 +0200

    masonry

--- new_final_project/masonry.js
+++ new_final_project/masonry.js
@@ -3,3 +3,15 @@ $('.grid').masonry({
   itemSelector: '.grid-item',
   columnWidth: 200
 });
+$('#container').imagesLoaded( function() {
+  // images have loaded
+});
+
+// options
+$('#container').imagesLoaded( {
+  // options...
+  },
+  function() {
+    // images have loaded
+  }
+);
