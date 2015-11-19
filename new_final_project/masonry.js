6e3e3bf3a31a332653aec37359f3c9a7816242b9
emmiliia <emmi-liia@torkkujajanokkosia.com>
2015-11-19 12:43:18 +0200

    short

--- new_final_project/masonry.js
+++ new_final_project/masonry.js
@@ -3,29 +3,3 @@ $('.grid').masonry({
   itemSelector: '.grid-item',
   columnWidth: 200
 });
-$('.grid').imagesLoaded( function() {
-  // images have loaded
-});
-
-// options
-$('.grid').imagesLoaded( {
-  // options...
-  },
-  function() {
-    // images have loaded
-  }
-);
-$('.grid').imagesLoaded()
-  .always( function( instance ) {
-    console.log('all images loaded');
-  })
-  .done( function( instance ) {
-    console.log('all images successfully loaded');
-  })
-  .fail( function() {
-    console.log('all images loaded, at least one is broken');
-  })
-  .progress( function( instance, image ) {
-    var result = image.isLoaded ? 'loaded' : 'broken';
-    console.log( 'image is ' + result + ' for ' + image.img.src );
-  });
