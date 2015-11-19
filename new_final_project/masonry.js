41660bef5a4d57ecb106992c14c80dedec9b821a
emmiliia <emmi-liia@torkkujajanokkosia.com>
2015-11-19 11:12:04 +0200

    images loaded

--- new_final_project/masonry.js
+++ new_final_project/masonry.js
@@ -15,3 +15,17 @@ $('#container').imagesLoaded( {
     // images have loaded
   }
 );
+$('#container').imagesLoaded()
+  .always( function( instance ) {
+    console.log('all images loaded');
+  })
+  .done( function( instance ) {
+    console.log('all images successfully loaded');
+  })
+  .fail( function() {
+    console.log('all images loaded, at least one is broken');
+  })
+  .progress( function( instance, image ) {
+    var result = image.isLoaded ? 'loaded' : 'broken';
+    console.log( 'image is ' + result + ' for ' + image.img.src );
+  });
