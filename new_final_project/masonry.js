29d9dd2e007dc9eefc107cc909bffa08fb920c55
emmiliia <emmi-liia@torkkujajanokkosia.com>
2015-11-30 23:04:14 +0200

    masonry

--- new_final_project/masonry.js
+++ new_final_project/masonry.js
@@ -1,5 +1,8 @@
-$('.grid').masonry({
+$(document).ready(function() {
+	var $grid =$('.grid').masonry({
   // options
   itemSelector: '.grid-item',
   columnWidth: 200
+});
+$grid.imagesLoaded().progress( function() {   $grid.masonry('layout'); });	
 });
